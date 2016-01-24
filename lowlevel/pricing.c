3d979f7bcedef99d22759c90c3dc38b0d5006f1b
arthur colle <hello@arthurcolle.com>
2016-01-24 01:29:48 -0500

    Added makefile for linking the GNU Scientific Library to the executable

--- lowlevel/pricing.c
+++ lowlevel/pricing.c
@@ -14,16 +14,24 @@ double price(
 )
 {
 	double sqrt_t = sqrt(t);
-	double d1 = (log(s/k)+r*t)/(v*sqrt_t);
-	double d2 = d1-(v*sqrt_t);
-	double exp_dt = exp(-d*t);
-	double exp_rt = exp(-r*t);
+	double rt = r*t;
+	double vsqrt_t = v*sqrt_t;
+	double d1 = (log(s/k)+rt)/vsqrt_t;
+	double d2 = d1 - vsqrt_t;
+	double dt = d*t;
+	double exp_dt = exp(-dt);
+	double exp_rt = exp(-rt);
 	double price = 0;
-
-	if (cp_flag == 'c')
-		price = s*exp_dt*gsl_cdf_ugaussian_P(d1)-k*exp_rt*gsl_cdf_ugaussian_P(d2);
-	if (cp_flag == 'p')
-		price = k*exp_rt*gsl_cdf_ugaussian_P(-d2)-s*exp_dt*gsl_cdf_ugaussian_P(-d1);
+	swith(cp_flag) {
+		case 'c': 
+			price = s*exp_dt*gsl_cdf_ugaussian_P( d1 ) - k*exp_rt*gsl_cdf_ugaussian_P(d2);
+			break;
+		case 'p': 
+			price = k*exp_rt*gsl_cdf_ugaussian_P( -d2 ) - s*exp_dt*gsl_cdf_ugaussian_P(-d1);
+			break;
+		default:
+			printf("Unacceptable option type.\nPlease specify 'c' or 'p'.\n");
+	}
 	return price;
 }
 
@@ -31,12 +39,24 @@ double price(
 int
 main (void)
 {
-  double s = 87.5;
-  double k = 85;
-  double r = 0.005;
-  double t = 0.04;
-  double v = 0.06;
-  double d = 0;
+  double underlier_price = 87.5;
+  double s = underlier_price;
+
+  double strike_price = 85;
+  double k = strike_price;
+
+  double risk_free_rate = 0.005;
+  double r = risk_free_rate;
+
+  double time_to_maturity = 0.04;
+  double t = time_to_maturity;
+
+  double underlier_volatility = 0.06;
+  double v = underlier_volatility;
+
+  double discount_rate = 0;
+  double d = discount_rate;
+
   printf("Underlier price: %f\n", s);
   printf("   Strike price: %f\n", k);
   printf(" risk free rate: %0.05f\n", r);
