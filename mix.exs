2e892a067e95d7dbd1f33caa57151fc875af6604
arthur colle <hello@arthurcolle.com>
2016-01-24 01:36:56 -0500

    Added LICENSE.md

--- mix.exs
+++ mix.exs
@@ -17,15 +17,13 @@ defmodule Pricing.Mixfile do
     [applications: [:logger, :tzdata]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [ files: ["lib", "priv", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Paul Schoenfelder"],
+      licenses: ["MIT"],
+      links: %{ "GitHub": "https://github.com/arthurcolle/pricing" } ]
+  end
+
   defp deps do
     [
       {:timex, "~> 1.0.0"}
