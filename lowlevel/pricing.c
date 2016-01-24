542ff1c6ba28c258be8dfa64d2d06667240054ff
arthur colle <hello@arthurcolle.com>
2016-01-23 23:01:46 -0500

    Added better printing with the parameters to the pricing function

--- lowlevel/pricing.c
+++ lowlevel/pricing.c
@@ -16,11 +16,15 @@ double price(
 	double sqrt_t = sqrt(t);
 	double d1 = (log(s/k)+r*t)/(v*sqrt_t);
 	double d2 = d1-(v*sqrt_t);
+	double exp_dt = exp(-d*t);
+	double exp_rt = exp(-r*t);
+	double price = 0;
+
 	if (cp_flag == 'c')
-		return s*exp(-d*t)*gsl_cdf_ugaussian_P(d1)-k*exp(-r*t)*gsl_cdf_ugaussian_P(d2);
-	
+		price = s*exp_dt*gsl_cdf_ugaussian_P(d1)-k*exp_rt*gsl_cdf_ugaussian_P(d2);
 	if (cp_flag == 'p')
-		return k*exp(-r*t)*gsl_cdf_ugaussian_P(-d2)-s*exp(-d*t)*gsl_cdf_ugaussian_P(-d1);
+		price = k*exp_rt*gsl_cdf_ugaussian_P(-d2)-s*exp_dt*gsl_cdf_ugaussian_P(-d1);
+	return price;
 }
 
 
@@ -33,9 +37,16 @@ main (void)
   double t = 0.04;
   double v = 0.06;
   double d = 0;
+  printf("Underlier price: %f\n", s);
+  printf("   Strike price: %f\n", k);
+  printf(" risk free rate: %0.05f\n", r);
+  printf(" Time to expiry: %0.05f\n", t);
+  printf("     Volatility: %0.05f\n", v);
+  printf("  Discount rate: %0.05f\n", d);
   
-  printf("price c: %f\n", price('c', s,k,r,v,t,d));
-  printf("price p: %f\n", price('p', s,k,r,v,t,d));
+  printf("\n");
+  printf("price call: %f\n", price('c', s,k,r,v,t,d));
+  printf("price  put: %f\n", price('p', s,k,r,v,t,d));
   
   return 0;
 }
