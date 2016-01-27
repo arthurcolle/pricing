a0a588623d445b5ec2972055da4a723cf4cd9ea4
arthur colle <hello@arthurcolle.com>
2016-01-27 01:05:37 -0500

    Returns a map of prices

--- lib/pricing.ex
+++ lib/pricing.ex
@@ -1,28 +1,27 @@
 defmodule Pricing do
 	def option(opts \\ []) do
-		[price: p, strike: k, rate: r, t: t, vol: v, dividend_yield: d] = opts
+		[price: p, strike: k, rate: r, t: t, vol: v, dy: d] = opts
 		%Porcelain.Result{err: err, out: out} = Porcelain.exec("./pricing.exe", [p, k, r, t, v, d])
 		out
 	end
 
-	def bitcoin do
+	def bitcoin_option do
 		price = Integer.to_string(300)
 		strike = Integer.to_string(250)
 		rate = Float.to_string(0.005, [decimals: 3, compact: true])
 		t = Float.to_string(0.75, [decimals: 2, compact: true])
 		v = Float.to_string(0.75, [decimals: 2, compact: true])
-		dy = Float.to_string(0.00, [decimals: 2, compact: true])
+		dy = Float.to_string(0.001, [decimals: 2, compact: true])
 
-		Pricing.option([	
-			price: price,
-			strike: strike, 
-			rate: rate,
-			t: t,
-			vol: v, 
-			dividend_yield: dy
-		]) 
+		components = 
+			[{:price, price},
+			 {:strike, strike}, 
+			 {:rate, rate},
+       {:t, t},
+       {:vol, v}, 
+       {:dy, dy}]
+
+		[c,p] = Poison.decode!(Pricing.option(components))
+		%{call: c, put: p}
 	end
 end
-
-
-[price: 123, x: 119]
